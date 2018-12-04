a4f7476b3cc6b9955ca6e8fb17097718b1d975f4
MiramON <ordonez.nunan.miriam@gmail.com>
2018-12-04 17:33:52 +0100

    Clases Teléfono, direccion, Provincia

---
+++ Agenda/src/main/java/spring/model/Provincia.java
@@ -0,0 +1,12 @@
+package spring.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.Id;
+
+public class Provincia {
+
+}
