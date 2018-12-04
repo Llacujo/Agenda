a0cfa34d3387f335c1dfb8c5fbf64e03c9849403
Andoni84 <45061143+Andoni84@users.noreply.github.com>
2018-12-04 16:25:26 +0100

    TestList y Carcasa clase AgendaController

---
+++ Agenda/src/test/java/spring/AgendaControllerListTest.java
@@ -0,0 +1,26 @@
+package spring;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import spring.controller.AgendaController;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AgendaControllerListTest {
+
+	@Autowired
+	AgendaController control;
+
+	@Test
+	public void devuelveLista() {
+		 
+		assertThat(control.list(Contacto.class)).isList();
+		
+	}
+}
