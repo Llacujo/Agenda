ca52d75731b61cb6c0f584b6ab699d4ce8762a57
MiramON <ordonez.nunan.miriam@gmail.com>
2018-12-05 15:44:08 +0100

    Actualizado el paquete services
    Incorporación del método listar(); en IServices y en ServicesAbstract

---
+++ Agenda/src/main/java/spring/services/IServices.java
@@ -0,0 +1,12 @@
+package spring.services;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface IServices <T extends Serializable>{
+	
+	void setClazz (Class <T> clazzToSet);
+	
+	List<T> listar();
+
+}
---
+++ Agenda/src/main/java/spring/services/ServicesAbstract.java
@@ -0,0 +1,51 @@
+package spring.services;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import spring.dao.IDAO;
+
+
+/**
+ * Clase ServicesAbstract
+ * 
+ * Clase generica para realizar
+ * 
+ * @author GrupoA
+ * @version 1.0 
+ */
+
+
+public abstract class ServicesAbstract<T extends Serializable> {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ServicesAbstract.class);
+	
+	private Class<T> clazz;
+	
+	@Autowired 
+	IDAO<T> dao;
+		
+	/**
+	 * Asigna una entidad al tipo generico
+	 * 
+	 * @param clase de una entidad
+	 */
+	public void setClazz(Class<T> clazzToSet) {
+		logger.debug("Asignamdo clase"+clazzToSet+"a Services");
+		this.clazz=clazzToSet;
+		dao.setClazz(clazzToSet);
+	}
+	
+	public List<T> listar(){
+		logger.warn("service: listar"+clazz.getName());
+		return dao.listar();
+	}
+	
+
+	
+	
+}
