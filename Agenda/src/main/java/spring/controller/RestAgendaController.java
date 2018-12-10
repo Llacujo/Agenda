ee8b1a8db4465cf594f022e2afdd5fcb23bf30e0
Llacujo <Llacujo@users.noreply.github.com>
2018-12-10 12:32:56 +0100

    anadirR

---
+++ Agenda/src/main/java/spring/controller/RestAgendaController.java
@@ -0,0 +1,37 @@
+package spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import spring.model.Contacto;
+import spring.services.IServices;
+
+@RestController
+public class RestAgendaController {
+	@Autowired
+	IServices servicio;
+	
+	/**
+     * Devuelve un contacto mediante un id a traves de la capa services
+     * @param id: id de usuario
+     * @return Contacto: devuelve un contacto
+     */
+	@RequestMapping(value="/detalleR", method=RequestMethod.GET)
+	public Contacto detallar(@RequestParam("id") int id) {
+		return	 (Contacto) servicio.detallar(Contacto.class, id);
+	}
+	/**
+	 * Crea un nuevo contacto
+	 */
+	@RequestMapping(value="/anadirR", method=RequestMethod.GET)
+	public void anadir() {
+		servicio.anadir(new Contacto());
+	}
+	
+}
